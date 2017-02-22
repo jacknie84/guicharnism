041522e54b6f4868297d3f54a014d2faea8fc357
정현기 <jacknie0@naver.com>
2017-02-22 13:05:58 +0900

    코드 가독성 향상 작업

--- guicharnism-mybatis-spring/src/main/java/com/jacknie/guicharnism/mybatis/NioMapperResourceWatcher.java
+++ guicharnism-mybatis-spring/src/main/java/com/jacknie/guicharnism/mybatis/NioMapperResourceWatcher.java
@@ -16,6 +16,7 @@ package com.jacknie.guicharnism.mybatis;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
@@ -35,17 +36,16 @@ import org.springframework.util.PathMatcher;
 
 public class NioMapperResourceWatcher implements MapperResourceWatcher {
 
-	private static final Logger logger = LoggerFactory.getLogger(NioMapperResourceWatcher.class);
+	private final Logger logger = LoggerFactory.getLogger(NioMapperResourceWatcher.class);
+	private final PathMatcher pathMatcher = new AntPathMatcher();
 	
 	private final MapperResourceWatchContext watchContext;
 	private final Configuration configuration;
 	private final String realoadTargetFilePattern;
-	private final PathMatcher pathMatcher;
 	
 	public NioMapperResourceWatcher(MapperResourceWatchContext watchContext, Configuration configuration, String realoadTargetFilePattern) {
 		this.watchContext = watchContext;
 		this.configuration = configuration;
-		this.pathMatcher = new AntPathMatcher();
 		if (pathMatcher.isPattern(realoadTargetFilePattern)) {
 			this.realoadTargetFilePattern = realoadTargetFilePattern;
 		}
@@ -68,60 +68,78 @@ public class NioMapperResourceWatcher implements MapperResourceWatcher {
 		if (!watchTargetDirectory.isDirectory()) {
 			throw new IllegalArgumentException("File object is not referenced directory.");
 		}
-		
-		final Path watchTarget = Paths.get(watchTargetDirectory.toURI());
-		
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						WatchService watcher = watchTarget.getFileSystem().newWatchService();
-						watchTarget.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY);
-						WatchKey watchKey = watcher.take();
-						List<WatchEvent<?>> events = watchKey.pollEvents();
-						for (WatchEvent<?> event : events) {
-							if (StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind())) {
-								handleEvent(watchTarget, event);
-							}
-						}
-					} catch (IOException | InterruptedException e) {
-						throw new IllegalStateException(e);
-					}
-				}
-			}
-			
-		}).start();
+		Runnable watchRunner = new WatchRunner(watchTargetDirectory);
+		Thread watchThread = new Thread(watchRunner);
+		watchThread.start();
 	}
 	
-	private void handleEvent(Path watchTarget, WatchEvent<?> event) throws InterruptedException, IOException {
-		Path eventPath = (Path) event.context();
-		String fileName = eventPath.getFileName().toString();
-		if (isTargetFileName(fileName)) {
-			Path resolvedPath = watchTarget.resolve(eventPath).toAbsolutePath();
-			logger.debug("[{}] file modified.", resolvedPath);
-			List<Resource> resources = watchContext.getResources(watchTarget.toString());
-			if (resources != null) {
-				for (Resource r : resources) {
-					Path resourcePath = Paths.get(r.getURI());
-					if (resourcePath.equals(resolvedPath)) {
-						logger.debug("start parse mapper resource.");
-						try {
-							XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(r.getInputStream(),
-									configuration, r.toString(), configuration.getSqlFragments());
+	private class WatchRunner implements Runnable {
 		
-							xmlMapperBuilder.parse();
-						} catch (Exception e) {
-							e.printStackTrace();
-						} finally {
-							ErrorContext.instance().reset();
+		private Path watchTargetPath;
+		
+		WatchRunner(File watchTargetDirectory) {
+			this(Paths.get(watchTargetDirectory.toURI()));
+		}
+
+		WatchRunner(Path watchTargetPath) {
+			this.watchTargetPath = watchTargetPath;
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					WatchService watcher = watchTargetPath.getFileSystem().newWatchService();
+					watchTargetPath.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY);
+					WatchKey watchKey = watcher.take();
+					List<WatchEvent<?>> events = watchKey.pollEvents();
+					for (WatchEvent<?> event : events) {
+						if (StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind())) {
+							handleEvent(event);
 						}
-						logger.debug("WatcherService reloads mybatis mapper resource.");
-						break;
 					}
+					
+				} catch (IOException | InterruptedException e) {
+					throw new IllegalStateException(e);
 				}
 			}
 		}
+		
+		void handleEvent(WatchEvent<?> event) throws IOException {
+			Path eventPath = (Path) event.context();
+			Path resolvedPath = watchTargetPath.resolve(eventPath).toAbsolutePath();
+			logger.debug("[{}] file modified.", resolvedPath);
+			Resource mapperResource = getMatchedMapperResource(resolvedPath);
+			if (mapperResource != null) {
+				logger.debug("start parse mapper resource.");
+				try {
+					InputStream mapperSource = mapperResource.getInputStream();
+					String resourceName = mapperResource.toString();
+					XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(mapperSource,
+							configuration, resourceName, configuration.getSqlFragments());
+	
+					xmlMapperBuilder.parse();
+				} catch (Exception e) {
+					throw new IllegalStateException(e);
+				} finally {
+					ErrorContext.instance().reset();
+				}
+				logger.debug("WatcherService reloads mybatis mapper resource.");
+			}
+		}
+		
+		Resource getMatchedMapperResource(Path resolvedPath) throws IOException {
+			List<Resource> resources = watchContext.getResources(watchTargetPath.toString());
+			if (resources != null) {
+				for (Resource resource : resources) {
+					Path resourcePath = Paths.get(resource.getURI());
+					if (resourcePath.equals(resolvedPath)) {
+						return resource;
+					}
+				}
+			}
+			return null;
+		}
+		
 	}
 }
